d7e5c637ea4fba02e569bdf1056c027913a91899
Asher Sandler <52999326+asher-sandler@users.noreply.github.com>
2021-12-14 22:13:18 +0200

    2021-12-14

---
+++ install.cmd
@@ -0,0 +1,114 @@
+rem Material Design Lite
+rem https://tleunen.github.io/react-mdl/
+rem https://material-ui.com/api
+rem https://semantic-ui.com/
+
+rem https://www.dafont.com/
+
+rem https://material.io/resources/icons/?style=baseline
+
+
+rem UI Gradients
+rem https://uigradients.com/
+rem http://hex2rgba.devoth.com/
+rem http://loading.io
+rem https://adaptivecards.io
+rem https://adaptivecards.io/designer/
+
+rem font awesome CDN
+rem https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css
+
+
+rem https://fontawesome.com/v4.7.0/icons/
+
+rem http://fonts.google.com
+
+rem https://www.shareicon.net/
+
+rem materializecss.com
+rem animate.css
+rem office-ui-fabric
+
+rem https://css-tricks.com/snippets/css/a-guide-to-flexbox/
+rem https://getbootstrap.com/docs/5.1/utilities/flex/
+
+
+yarn add react-mdl
+yarn add react-router-dom
+
+npm i -g firebase-tools
+
+npm run build
+firebase login
+firebase init
+firebase deploy
+
+
+через несколько месяцев пришлось делать:
+firebase logout
+firebase login
+
+Add path to firebase.cmd
+c:\Users\smf70\AppData\Roaming\npm\
+
+https://www.give.org.il/campaign/ramot/
+
+
+Project Console: https://console.firebase.google.com/project/ashersandler-resume/overview
+Hosting URL: https://ashersandler-resume.web.app
+
+https://ashersandler-resume.firebaseapp.com
+================
+REDUX
+https://redux.js.org
+
+redux thunk
+
+npm install redux-thunk
+
+redux-logger
+
+combineReducer
+
+redux-devtools-extension
+================
+
+
+https://docs.google.com/spreadsheets/d/1tb78VZXh0Qwb23frGfbTfyj_259Epo3OiQgLdgbM-jM/edit#gid=0
+
+
+https://spreadsheets.google.com/feeds/list/1tb78VZXh0Qwb23frGfbTfyj_259Epo3OiQgLdgbM-jM/1/public/values?alt=json fetches first worksheet
+https://spreadsheets.google.com/feeds/list/1tb78VZXh0Qwb23frGfbTfyj_259Epo3OiQgLdgbM-jM/2/public/values?alt=json 2nd sheet
+https://spreadsheets.google.com/feeds/list/1tb78VZXh0Qwb23frGfbTfyj_259Epo3OiQgLdgbM-jM/3/public/values?alt=json 3rd sheet and so on
+
+
+TestGmailConnection
+https://docs.google.com/spreadsheets/d/1LlgRfnPxVeFXL4SuV-QVapjl6EBWfzJ6EsJS-NrdRYg/edit?alt=json#gid=0
+
+https://spreadsheets.google.com/feeds/list/1LlgRfnPxVeFXL4SuV-QVapjl6EBWfzJ6EsJS-NrdRYg/public/values?alt=json
+
+https://docs.google.com/spreadsheets/d/1LlgRfnPxVeFXL4SuV-QVapjl6EBWfzJ6EsJS-NrdRYg/edit?usp=sharing
+https://spreadsheets.google.com/feeds/list/1LlgRfnPxVeFXL4SuV-QVapjl6EBWfzJ6EsJS-NrdRYg/od6/public/values?alt=json
+
+
+https://docs.google.com/spreadsheets/d/e/2PACX-1vTGIdVoY_EFzFlbreqfmYGrl1bOAYVkxwS19yIiwAdGqFXokkzKB2wVnE0n9p2FhvDIfWioQF195ehQ/pubhtml?alt=json
+
+
+-------------------------  TMURA -------------------------
+TMURA -SHEMA
+https://spreadsheets.google.com/feeds/worksheets/1G1jbWnthjTNbqGlULWhJN4xmjZwodPN9z6U8QXcA6CU/private/full
+
+TAB1 - TMURA
+https://spreadsheets.google.com/feeds/list/1G1jbWnthjTNbqGlULWhJN4xmjZwodPN9z6U8QXcA6CU/od6/public/values?alt=json
+
+TAB1 - CONFIG
+https://spreadsheets.google.com/feeds/list/1G1jbWnthjTNbqGlULWhJN4xmjZwodPN9z6U8QXcA6CU/obg94ui/public/values?alt=json
+
+
+
+https://docs.google.com/spreadsheets/d/e/2PACX-1vTAnQm942rCEIFrQIFJecPEv4w8hNvSHB7H5md5Ulsir9My2RiQCwspwzDmm7m7OJLbL4df9WBqZ4RW/pubhtml
+
+https://docs.google.com/spreadsheets/d/1bIVotqMEt1Ki8h3s1FzV5ZcDjqEO0Jp1JeC0T-2diio/edit?usp=sharing
+
+TRUMA - TEST
+https://spreadsheets.google.com/feeds/list/1bIVotqMEt1Ki8h3s1FzV5ZcDjqEO0Jp1JeC0T-2diio/1/public/values?alt=json
